8e8ff48396b71ce4baa146a15275583bce64ef11
Fredrik Noren <fredrik.jw.noren@gmail.com>
2016-04-10 02:24:30 -0400

    Link with stdc++

--- build.rs
+++ build.rs
@@ -3,4 +3,5 @@ use std::env;
 fn main() {
     let module_path = env::var("CARGO_MANIFEST_DIR").unwrap();
     println!("cargo:rustc-link-search=native={}", module_path);
+    println!("cargo:rustc-link-lib=static=stdc++");
 }
