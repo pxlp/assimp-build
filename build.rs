b469880aa3ec2dcb070fa0ba22d8a15340599699
Fredrik Noren <fredrik.jw.noren@gmail.com>
2016-04-10 11:42:04 -0400

    Build using cargo build script

--- build.rs
+++ build.rs
@@ -1,7 +1,42 @@
+extern crate cmake;
+extern crate pkg_config;
+
+use cmake::Config;
 use std::env;
+use std::process::Command;
+use std::path::Path;
+use std::fs;
+use std::io::ErrorKind;
 
 fn main() {
-    let module_path = env::var("CARGO_MANIFEST_DIR").unwrap();
-    println!("cargo:rustc-link-search=native={}", module_path);
-    println!("cargo:rustc-link-lib=static=stdc++");
+    let target = env::var("TARGET").unwrap();
+    let generator = env::var("GENERATOR");
+
+    // Compile assimp from source
+    // Disable unnecessary stuff, it takes long enough to compile already
+    let mut cfg = Config::new("assimp");
+    cfg.define("ASSIMP_BUILD_ASSIMP_TOOLS", "OFF")
+        .define("ASSIMP_BUILD_TESTS", "OFF")
+        .define("BUILD_SHARED_LIBS", "OFF")
+        .define("CMAKE_BUILD_TYPE", "Release")
+        .define("LIBRARY_SUFFIX", "");
+    if let Ok(generator) = generator {
+        cfg.generator(generator);
+    }
+    let dst = cfg.build();
+    println!("cargo:rustc-link-search=native={}", dst.join("lib").display());
+
+    // Link to correct versions of assimp and zlib
+    // NOTE: MSVC has to link to release libs to avoid CRT mismatch
+    println!("cargo:rustc-link-lib=static=assimp");
+    if !pkg_config::find_library("zlib").is_ok() {
+        println!("cargo:rustc-link-lib=static=zlibstatic");
+    }
+
+    // Link to libstdc++ on GNU
+    if target.contains("gnu") {
+        println!("cargo:rustc-link-lib=stdc++");
+    }
+
+    println!("cargo:rerun-if-changed=build.rs");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -4,5 +4,5 @@ extern crate assimp;
 fn it_works() {
     let mut importer = assimp::Importer::new();
     let scene = importer.read_file("Barrel1.x").unwrap();
-    panic!("{:?}", scene.num_meshes());
+    assert_eq!(scene.num_meshes(), 1);
 }
